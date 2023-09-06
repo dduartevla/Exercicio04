257903735ecb6fdacbc4b00e7db30bf4dbf1e011
dduartevla <deboraduarte@ice.ufjf.br>
2023-09-05 21:22:54 -0300

    Cria classe Aluno

---
+++ src/main/java/Aluno.java
@@ -0,0 +1,26 @@
+public class Aluno {
+    private  String nome;
+    private String nomeCurso;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
+
+    public String imprimirDiploma(){
+        String coisaPraImprimir;
+
+        return coisaPraImprimir;
+    }
+}
